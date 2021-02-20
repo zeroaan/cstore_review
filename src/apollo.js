3c288a8d46e9024085fcc37c855b1a8f0251f3e1
zeroaan <yj6210ag@gmail.com>
2021-02-20 18:58:23 +0900

    Update: apollo uri

--- src/apollo.js
+++ src/apollo.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client"
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: "http://localhost:4000/",
+  uri: "https://cstore-server.herokuapp.com/",
 })
 
 export default client
