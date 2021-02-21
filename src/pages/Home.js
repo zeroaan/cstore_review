31e318c2c7fdc1010a389d107554c10b2ce1940b
zeroaan <yj6210ag@gmail.com>
2021-02-21 15:07:22 +0900

    Update: best 상품 함수 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,17 +16,15 @@ const Home = () => {
       [...foods].sort((a, b) => {
         let sumA = 0,
           sumB = 0,
-          resultA,
-          resultB
+          resultA = 0,
+          resultB = 0
         a.review.map((v) => (sumA += v.star))
         b.review.map((v) => (sumB += v.star))
-        a.review.length === 0
-          ? (resultA = 0)
-          : (resultA = sumA / a.review.length)
-        b.review.length === 0
-          ? (resultB = 0)
-          : (resultB = sumB / b.review.length)
-        return resultB - resultA
+        a.review.length !== 0 && (resultA = sumA / a.review.length)
+        b.review.length !== 0 && (resultB = sumB / b.review.length)
+        return (
+          resultB + b.liked.length * 0.01 - (resultA + a.liked.length * 0.01)
+        )
       })
     )
   }
